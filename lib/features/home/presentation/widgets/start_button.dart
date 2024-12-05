c3c5799e732cf0fa31d5a7000c5c2010165ee6c1
kihong <rlghd1698@gmail.com>
2024-12-05 23:10:36 +0900

    refactor(home): start 버튼 디자인 변경

--- lib/features/home/presentation/widgets/start_button.dart
+++ lib/features/home/presentation/widgets/start_button.dart
@@ -8,7 +8,8 @@ class StartButton extends StatelessWidget {
     return Expanded(
       child: Card(
         margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-        color: Theme.of(context).cardColor,
+        elevation: 0,
+        color: Theme.of(context).colorScheme.primaryContainer,
         child: Center(
           child: Column(
             mainAxisSize: MainAxisSize.min,
@@ -16,19 +17,29 @@ class StartButton extends StatelessWidget {
               Container(
                 width: 120,
                 height: 120,
+                margin: const EdgeInsets.only(bottom: 8.0),
                 decoration: BoxDecoration(
-                  color: Colors.white,
+                  color: Theme.of(context).secondaryHeaderColor,
+                  // color: Colors.white,
                   borderRadius: BorderRadius.circular(100.0),
-                  border: Border.all(color: Colors.black, width: 4),
+                  // border: Border.all(color: Colors.black, width: 4),
                 ),
                 child: IconButton(
                   onPressed: () {
                     // TODO: 문제 풀이 화면으로 이동
                   },
-                  icon: const Icon(Icons.play_arrow, size: 60),
+                  icon: Icon(
+                    Icons.play_arrow,
+                    size: 60,
+                    color: Theme.of(context).primaryColor,
+                  ),
                 ),
               ),
-              Text('Start', style: Theme.of(context).textTheme.displayLarge),
+              const SizedBox(height: 10),
+              Text(
+                '문제 풀기',
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
             ],
           ),
         ),
