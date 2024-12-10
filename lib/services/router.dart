84ad646e3ae7fbd448bcc1e08ae007e58a22272f
kihong <rlghd1698@gmail.com>
2024-12-10 23:20:26 +0900

    fix(home): 누락된 라우터 추가

--- lib/services/router.dart
+++ lib/services/router.dart
@@ -2,6 +2,7 @@ import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:math_skill_up/features/basic_calculation/presentation/basic_calculation_page.dart';
 import 'package:math_skill_up/features/history/presentation/history_screen.dart';
+import 'package:math_skill_up/features/home/presentation/home_screen.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'router.g.dart';
@@ -11,6 +12,10 @@ GoRouter router(Ref ref) {
   final router = GoRouter(
     initialLocation: '/basic-calulation', //TODO: 임시로 변경, home으로 변경되어야함
     routes: [
+      GoRoute(
+        path: '/',
+        builder: (_, __) => const HomeScreen(),
+      ),
       GoRoute(
         path: '/basic-calulation',
         builder: (_, __) => const BasicCalculationPage(),
