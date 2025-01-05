74cc48669e7d351f1e20eb6ffc9c924295cef44b
sunmin <polarlsm1231@gmail.com>
2025-01-05 14:19:15 +0900

    feat: 퍼센트 비교시 한쪽은 분수로 변경 #15

--- lib/features/question/presentation/widgets/question_box/fraction_question_box.dart
+++ lib/features/question/presentation/widgets/question_box/fraction_question_box.dart
@@ -5,6 +5,8 @@ import 'package:math_skill_up/features/question/presentation/widgets/question_bo
 import 'package:math_skill_up/features/question/presentation/widgets/question_box/fraction_widget.dart';
 import 'package:math_skill_up/features/question_setting/model/question_setting_model.dart';
 
+const double _blankBoxWidth = 50;
+
 class FractionQuestionBox extends ConsumerWidget {
   const FractionQuestionBox({super.key, required this.question});
 
@@ -12,36 +14,38 @@ class FractionQuestionBox extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Stack(
       children: [
-        question.type == FractionType.fraction
-            ? FractionWidget(
-                fraction: question.firstFraction,
-              )
-            : Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 20), // 왼쪽 여백
-                  child: Text(
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: EdgeInsets.only(
+                left:
+                    MediaQuery.of(context).size.width / 6 - _blankBoxWidth / 2),
+            child: question.type == FractionType.fraction
+                ? FractionWidget(
+                    fraction: question.firstFraction,
+                  )
+                : Text(
                     '${question.firstFraction.percent}%',
                     style: Theme.of(context).textTheme.displayMedium,
                   ),
-                ),
-              ),
-        const SizedBox(width: 20), // Text 사이에 마진 추가
-        const BlankBox(
-          width: 50,
+          ),
         ),
-        const SizedBox(width: 20), // Text 사이에 마진 추가
-        question.type == FractionType.fraction
-            ? FractionWidget(
+        const Center(
+          child: BlankBox(
+            width: _blankBoxWidth,
+          ),
+        ),
+        Align(
+          alignment: Alignment.centerRight,
+          child: Padding(
+              padding:
+                  EdgeInsets.only(right: MediaQuery.of(context).size.width / 6),
+              child: FractionWidget(
                 fraction: question.secondFraction,
-              )
-            : Text(
-                '${question.secondFraction.percent}%',
-                style: Theme.of(context).textTheme.displayMedium,
-              ),
+              )),
+        ),
       ],
     );
   }
