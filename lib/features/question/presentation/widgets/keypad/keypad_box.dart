bbed42da4892b19c3db316578cdbf239b1a5adf9
sunmin <polarlsm1231@gmail.com>
2025-02-27 19:54:46 +0900

    feat: keypad ui 구현 #13

--- lib/features/question/presentation/widgets/keypad/keypad_box.dart
+++ lib/features/question/presentation/widgets/keypad/keypad_box.dart
@@ -8,16 +8,55 @@ class KeypadBox extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final buttons = [
+      '7',
+      '8',
+      '9',
+      '4',
+      '5',
+      '6',
+      '1',
+      '2',
+      '3',
+      '.',
+      '0',
+      '⌫'
+    ];
     return Container(
       height: 340,
       color: AppColors.white,
       child: Column(
         children: [
           const KeypadHeader(),
-          Text(
-            "Keypad",
-            style: Theme.of(context).textTheme.displayLarge,
-          )
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                mainAxisSpacing: 8.0,
+                crossAxisSpacing: 8.0,
+                childAspectRatio: 2, // 정사각형 비율로 설정
+              ),
+              itemCount: buttons.length,
+              itemBuilder: (context, index) {
+                return ElevatedButton(
+                  onPressed: () {
+                    // 버튼 클릭 시 동작 추가
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black,
+                  ),
+                  child: Text(
+                    buttons[index],
+                    style: Theme.of(context).textTheme.displayMedium,
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
