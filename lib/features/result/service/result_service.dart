5ef5fbdd53e46318ed357fc8d45cb742cdf95438
kihong <rlghd1698@gmail.com>
2025-03-11 00:11:12 +0900

    feat(result): 서비스 생성 #8

---
+++ lib/features/result/service/result_service.dart
@@ -0,0 +1,56 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:math_skill_up/features/history/model/history_model.dart';
+import 'package:math_skill_up/features/history/repository/history_repository.dart';
+import 'package:math_skill_up/features/history/repository/hive_history_repository.dart';
+import 'package:math_skill_up/features/question_setting/model/question_setting_model.dart';
+import 'package:math_skill_up/features/result/model/result_model.dart';
+import 'package:math_skill_up/features/result/repository/result_list_repository.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+@riverpod
+ResultService resultService(Ref ref) {
+  return ResultService(ref: ref);
+}
+
+class ResultService {
+  final Ref ref;
+
+  ResultService({
+    required this.ref,
+  });
+
+  ResultSummary? calResultSummary() {
+    List<Result> results = ref.watch(resultListRepositoryProvider);
+    if (results.isEmpty) return null;
+    History? latestHistory = getLatestHistory(results.first);
+    //TODO: 이어서 로직 작성해야함.
+    return null;
+  }
+
+  History? getLatestHistory(Result result) {
+    HistoryRepository historyRepository = ref.read(historyRepositoryProvider);
+    if (result is BasicResult) {
+      switch (result.question.operator) {
+        case ArithmeticType.addition:
+          return historyRepository.getLatestAdditionHistory();
+        case ArithmeticType.subtraction:
+          return historyRepository.getLatestSubtractionHistory();
+        case ArithmeticType.multiplication:
+          return historyRepository.getLatestMultiplicationHistory();
+        case ArithmeticType.division:
+          return historyRepository.getLatestDivisionHistory();
+      }
+    } else if (result is FractionResult) {
+      switch (result.question.type) {
+        case FractionType.fraction:
+          return historyRepository.getLatestFractionHistory();
+        case FractionType.percent:
+          return historyRepository.getLatestPercentHistory();
+      }
+    } else if (result is AlphabetResult) {
+      return historyRepository.getLatestAlphabetHistory();
+    }
+
+    throw ArgumentError('Unknown result type');
+  }
+}
